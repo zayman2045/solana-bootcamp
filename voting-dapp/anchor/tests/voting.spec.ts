80d225caa026e0ac6be869aa6aecfb52d54027d5
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-10 00:32:37 -0500

    Refactor voting tests to use beforeAll for context initialization and add candidate initialization tests

--- voting-dapp/anchor/tests/voting.spec.ts
+++ voting-dapp/anchor/tests/voting.spec.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Keypair, PublicKey } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import { Voting } from "../target/types/voting";
 import { BankrunProvider, startAnchor } from "anchor-bankrun";
 
@@ -11,16 +11,21 @@ const votingAddress = new PublicKey(
 );
 
 describe("Voting", () => {
-  it("Initialize Poll", async () => {
-    const context = await startAnchor(
+  let context;
+  let provider;
+  let votingProgram: Program<Voting>;
+
+  beforeAll(async () => {
+    context = await startAnchor(
       "",
       [{ name: "voting", programId: votingAddress }],
       []
     );
-    const provider = new BankrunProvider(context);
+    provider = new BankrunProvider(context);
+    votingProgram = new Program<Voting>(IDL, provider);
+  });
 
-    const votingProgram = new Program<Voting>(IDL, provider);
-
+  it("Initialize poll", async () => {
     await votingProgram.methods
       .initializePoll(
         new anchor.BN(1),
@@ -30,15 +35,51 @@ describe("Voting", () => {
       )
       .rpc();
 
-      const [pollAddress] = await PublicKey.findProgramAddressSync([new anchor.BN(1).toArrayLike(Buffer, "le", 8)], votingAddress);
+    const [pollAddress] = await PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAddress
+    );
 
-      const poll = await votingProgram.account.poll.fetch(pollAddress);
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
 
-      console.log(poll);
+    console.log(poll);
 
-      expect(poll.pollId.toNumber()).toEqual(1);
-      expect(poll.description).toEqual("Who is your favorite basketball player?");
-      expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
-
+    expect(poll.pollId.toNumber()).toEqual(1);
+    expect(poll.description).toEqual("Who is your favorite basketball player?");
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
   });
+
+  it("Initialize candidate", async () => {
+    await votingProgram.methods
+      .initializeCandidate("LeBron James", new anchor.BN(1))
+      .rpc();
+
+    await votingProgram.methods
+      .initializeCandidate("Stephen Curry", new anchor.BN(1))
+      .rpc();
+
+    const [lebronAddress] = await PublicKey.findProgramAddressSync(
+      [
+        new anchor.BN(1).toArrayLike(Buffer, "le", 8),
+        Buffer.from("LeBron James"),
+      ],
+      votingAddress
+    );
+
+    const lebronAccount = await votingProgram.account.candidate.fetch(lebronAddress);
+
+    const [curryAddress] = await PublicKey.findProgramAddressSync(
+      [
+        new anchor.BN(1).toArrayLike(Buffer, "le", 8),
+        Buffer.from("Stephen Curry"),
+      ],
+      votingAddress
+    );
+
+    const curryAccount = await votingProgram.account.candidate.fetch(curryAddress);
+
+    console.log(lebronAccount, curryAccount);
+  });
+
+  it("Vote", async () => {});
 });
