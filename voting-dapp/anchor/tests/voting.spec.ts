00564595db71a16b2354c41432dce071d578ee71
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-09 23:36:56 -0500

    Add unit test for initializing a poll in voting program

---
+++ voting-dapp/anchor/tests/voting.spec.ts
@@ -0,0 +1,33 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { Voting } from "../target/types/voting";
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+
+const IDL = require("../target/idl/voting.json");
+
+const votingAddress = new PublicKey(
+  "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF"
+);
+
+describe("Voting", () => {
+  it("Initialize Poll", async () => {
+    const context = await startAnchor(
+      "",
+      [{ name: "voting", programId: votingAddress }],
+      []
+    );
+    const provider = new BankrunProvider(context);
+
+    const votingProgram = new Program<Voting>(IDL, provider);
+
+    await votingProgram.methods
+      .initializePoll(
+        new anchor.BN(1),
+        "Who is your favorite basketball player?",
+        new anchor.BN(0),
+        new anchor.BN(1836469290)
+      )
+      .rpc();
+  });
+});
