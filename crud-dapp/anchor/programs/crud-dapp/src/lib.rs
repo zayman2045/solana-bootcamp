7ef5e428a0fd7a67ac8cdddf222ac5b1cc782d0f
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-11 18:46:16 -0500

    Implement create_entry function and associated data structures for CRUD operations

--- crud-dapp/anchor/programs/crud-dapp/src/lib.rs
+++ crud-dapp/anchor/programs/crud-dapp/src/lib.rs
@@ -8,4 +8,31 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod crud_dapp {
     use super::*;
 
+    pub fn create_entry(ctx: Context<CreateEntry>, title: String, message: String) -> Result<()> {
+        let entry = &mut ctx.accounts.entry;
+        entry.owner = ctx.accounts.user.key();
+        entry.title = title;
+        entry.message = message;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(init, payer = user,space = 8 + Entry::INIT_SPACE, seeds = [title.as_bytes(), user.key().as_ref()], bump)]
+    pub entry: Account<'info, Entry>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Entry {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub message: String,
 }
