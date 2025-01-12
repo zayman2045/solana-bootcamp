d732f6c809efe3b81563bf63202739a03a4e8bb8
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-12 09:45:53 -0500

    Refactor tests to align with renamed CRUD program and update entry creation logic

--- crud-dapp/anchor/tests/cruddapp.spec.ts
+++ crud-dapp/anchor/tests/cruddapp.spec.ts
@@ -1,76 +1,27 @@
-import * as anchor from '@coral-xyz/anchor'
-import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
-import {Cruddapp} from '../target/types/cruddapp'
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { Crud } from "anchor/target/types/crud";
 
-describe('cruddapp', () => {
+describe("Crud", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
 
-  const program = anchor.workspace.Cruddapp as Program<Cruddapp>
+  const crudProgram = anchor.workspace.Crud as Program<Crud>;
 
-  const cruddappKeypair = Keypair.generate()
+  it("Create entry", async () => {
+    // await crudProgram.methods.createEntry("My Title", "My Message").rpc();
 
-  it('Initialize Cruddapp', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        cruddapp: cruddappKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([cruddappKeypair])
-      .rpc()
+    const [entryAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("My Title"), payer.publicKey.toBytes()],
+      crudProgram.programId
+    );
 
-    const currentCount = await program.account.cruddapp.fetch(cruddappKeypair.publicKey)
+    const entryAccount = await crudProgram.account.entry.fetch(entryAddress);
 
-    expect(currentCount.count).toEqual(0)
-  })
-
-  it('Increment Cruddapp', async () => {
-    await program.methods.increment().accounts({ cruddapp: cruddappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.cruddapp.fetch(cruddappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Increment Cruddapp Again', async () => {
-    await program.methods.increment().accounts({ cruddapp: cruddappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.cruddapp.fetch(cruddappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement Cruddapp', async () => {
-    await program.methods.decrement().accounts({ cruddapp: cruddappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.cruddapp.fetch(cruddappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set cruddapp value', async () => {
-    await program.methods.set(42).accounts({ cruddapp: cruddappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.cruddapp.fetch(cruddappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the cruddapp account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        cruddapp: cruddappKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.cruddapp.fetchNullable(cruddappKeypair.publicKey)
-    expect(userAccount).toBeNull()
-  })
-})
+    expect(entryAccount.message).toEqual("My Message");
+    expect(entryAccount.title).toEqual("My Title");
+  });
+});
