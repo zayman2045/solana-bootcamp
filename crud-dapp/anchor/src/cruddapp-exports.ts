3faa284e1e2b6ef6550afccc2970ff5e1fddf95d
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-12 10:45:18 -0500

    Refactor exports and helper functions to align with renamed CRUD program

--- crud-dapp/anchor/src/cruddapp-exports.ts
+++ crud-dapp/anchor/src/cruddapp-exports.ts
@@ -1,29 +1,35 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
-import { AnchorProvider, Program } from '@coral-xyz/anchor'
-import { Cluster, PublicKey } from '@solana/web3.js'
-import CruddappIDL from '../target/idl/cruddapp.json'
-import type { Cruddapp } from '../target/types/cruddapp'
+import { AnchorProvider, Program } from "@coral-xyz/anchor";
+import { Cluster, PublicKey } from "@solana/web3.js";
+import CrudIDL from "../target/idl/crud.json";
+import type { Crud } from "../target/types/crud";
 
 // Re-export the generated IDL and type
-export { Cruddapp, CruddappIDL }
+export { Crud, CrudIDL };
 
 // The programId is imported from the program IDL.
-export const CRUDDAPP_PROGRAM_ID = new PublicKey(CruddappIDL.address)
+export const CRUD_PROGRAM_ID = new PublicKey(CrudIDL.address);
 
-// This is a helper function to get the Cruddapp Anchor program.
-export function getCruddappProgram(provider: AnchorProvider, address?: PublicKey) {
-  return new Program({ ...CruddappIDL, address: address ? address.toBase58() : CruddappIDL.address } as Cruddapp, provider)
+// This is a helper function to get the Crud Anchor program.
+export function getCrudProgram(provider: AnchorProvider, address?: PublicKey) {
+  return new Program(
+    {
+      ...CrudIDL,
+      address: address ? address.toBase58() : CrudIDL.address,
+    } as Crud,
+    provider
+  );
 }
 
-// This is a helper function to get the program ID for the Cruddapp program depending on the cluster.
-export function getCruddappProgramId(cluster: Cluster) {
+// This is a helper function to get the program ID for the Crud program depending on the cluster.
+export function getCrudProgramId(cluster: Cluster) {
   switch (cluster) {
-    case 'devnet':
-    case 'testnet':
-      // This is the program ID for the Cruddapp program on devnet and testnet.
-      return new PublicKey('coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF')
-    case 'mainnet-beta':
+    case "devnet":
+    case "testnet":
+      // This is the program ID for the Crud program on devnet and testnet.
+      return new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+    case "mainnet-beta":
     default:
-      return CRUDDAPP_PROGRAM_ID
+      return CRUD_PROGRAM_ID;
   }
 }
