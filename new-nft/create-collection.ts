a11201aa0e2f4b47b6d7fa49ddd29372a405d321
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-14 07:33:41 -0500

    Enhance create-collection script to include user keypair loading and airdrop functionality

--- new-nft/create-collection.ts
+++ new-nft/create-collection.ts
@@ -4,12 +4,41 @@ import {
   mplTokenMetadata,
 } from "@metaplex-foundation/mpl-token-metadata";
 
-import { airdropIfRequired, getExplorerLink, getKeypairFromFile} from "@solana-developers/helpers";
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
 
-import {createUmi} from "@metaplex-foundation/umi-bundle-defaults";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 
-import { Connection, LAMPORTS_PER_SOL, clusterApiUrl} from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
 
+import { keypairIdentity } from "@metaplex-foundation/umi";
+
+// Create a connection to the cluster
 const connection = new Connection(clusterApiUrl("devnet"));
 
-const user = getKeypairFromFile();
+// Load the user keypair (if empty, loads from the default path)
+const user = await getKeypairFromFile();
+
+// Request airdrop if required
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL
+);
+
+// Log the user public key
+console.log("Loaded user", user.publicKey.toBase58());
+
+// Create Umi instance
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("Set up Umi instance for user");
+
