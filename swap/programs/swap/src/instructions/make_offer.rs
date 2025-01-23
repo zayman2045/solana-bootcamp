becbfabb68b2c72b287a76076a53c951db039bfa
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-23 11:14:26 -0800

    Remove initialize instruction and refactor to add make_offer functionality

---
+++ swap/programs/swap/src/instructions/make_offer.rs
@@ -0,0 +1,47 @@
+use crate::constants::ANCHOR_DISCRIMINATOR;
+use crate::state::Offer;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+pub fn send_offered_tokens_to_vault() -> Result<()> {
+    // This is a stub function that will be filled in later
+    Ok(())
+}
+
+pub fn save_offer() -> Result<()> {
+    // This is a stub function that will be filled in later
+    Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction(id: u64)]
+pub struct MakeOffer<'info> {
+    // The maker of the offer
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account(mint::token_program = token_program)]
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(mint::token_program = token_program)]
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(mut, associated_token::mint = token_mint_a, associated_token::authority = maker, associated_token::token_program = token_program)]
+    pub token_maker_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(init, payer = maker, space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE, seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()], bump)]
+    pub offer: Account<'info, Offer>,
+
+    #[account(init, payer = maker, associated_token::mint = token_mint_a, associated_token::authority = offer, associated_token::token_program = token_program)]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    // Used to create accounts
+    pub system_program: Program<'info, System>,
+    // Used for mint accounts
+    pub token_program: Interface<'info, TokenInterface>,
+    // Maps associated token accounts to their mint
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
--- swap/programs/swap/src/lib.rs
+++ swap/programs/swap/src/lib.rs
@@ -15,7 +15,8 @@ declare_id!("24YcWJAoAxZ5NQPVo1fc7xQZMstrkUfUNysHTu2QM5Ve");
 pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(context: Context<MakeOffer>) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault()?;
+        instructions::make_offer::save_offer()
     }
 }
