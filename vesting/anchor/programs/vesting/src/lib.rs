20b76dcb2bb57bc1401a70d708f3d8c694fcb89e
zayman2045 <xavierorlandogriffith@gmail.com>
2025-01-24 11:48:45 -0800

    Add create_vesting_account function and VestingAccount struct with associated accounts

--- vesting/anchor/programs/vesting/src/lib.rs
+++ vesting/anchor/programs/vesting/src/lib.rs
@@ -2,9 +2,38 @@
 
 use anchor_lang::prelude::*;
 
+pub const ANCHOR_DISCRIMINATOR: usize = 8;
+
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod vesting {
     use super::*;
+
+    pub fn create_vesting_account(ctx: Context<CreateVestingAccount>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(init, payer = signer, space = ANCHOR_DISCRIMINATOR + VestingAccount::INIT_SPACE)]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
 }
